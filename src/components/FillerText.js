3e65661dd672bdfec3f66aebb5912456be7815eb
chandana M <chandu11288@gmail.com>
2018-06-21 02:11:19 +0000

    automatically backed up by learn

---
+++ src/components/FillerText.js
@@ -0,0 +1,9 @@
+import react from 'React'
+
+export class FillerText extends react.component{
+  render(){
+    return(
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+    )
+  }
+}
